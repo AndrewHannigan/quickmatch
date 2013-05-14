bb7886acd600adf22b9b6ab7973992bf4123cb04
Andrew Hannigan <Andrew.S.Hannigan@Gmail.com>
2013-05-14 12:15:32 -0400

    new dtest.bash

--- scripts/dtest.bash
+++ scripts/dtest.bash
@@ -1,5 +1,5 @@
-./dtest.out 100 99 2 ../../csv/dtest_N100d99interval2.csv &
-./dtest.out 1000 49 8 ../../csv/dtest_N1000d49interval8.csv &
-./dtest.out 10000 29 6 ../../csv/dtest_N10000d29interval6.csv &
-./dtest.out 100000 19 4 ../../csv/dtest_N100000d19interval4.csv &
+./dtest.out 100 20 2 ../../csv/dtest_N100d20interval2.csv &
+./dtest.out 1000 40 4 ../../csv/dtest_N1000d40interval4.csv &
+./dtest.out 10000 44 4 ../../csv/dtest_N10000d44interval4.csv &
+./dtest.out 50000 19 2 ../../csv/dtest_N50000d19interval4.csv &
 
