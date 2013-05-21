b73c834bfcd509963e1a7d5a999b4a3728acc208
Andrew S. Hannigan <hannigan@icecream.cs.dartmouth.edu>
2013-05-21 03:03:24 -0400

    allbash modified

--- scripts/ALL.bash
+++ scripts/ALL.bash
@@ -6,6 +6,6 @@
 #./ntest.out 3 25 51200 2 ../../csv/ntest_d3N25to51200interval2.csv &
 
 #./alttest.out 1000 3 ../../csv/alttest_N1000d3.csv & 
-#./alttest.out 10000 3 ../../csv/alttest_N10000d3.csv & 
+./alttest.out 10000 3 ../../csv/alttest_N10000d3.csv & 
 ./alttest.out 100000 3 ../../csv/alttest_N100000d3.csv & 
 
