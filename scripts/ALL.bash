0d8ce411646676e49827dccbcd513dc1e5d70f54
Andrew S. Hannigan <hannigan@icecream.cs.dartmouth.edu>
2013-05-21 02:41:58 -0400

    all.bash mod

--- scripts/ALL.bash
+++ scripts/ALL.bash
@@ -3,7 +3,7 @@
 ./dtest.out 10000 44 4 ../../csv/dtest_N10000d44interval4.csv &
 ./dtest.out 50000 19 2 ../../csv/dtest_N50000d19interval2.csv &
 
-./ntest.out 3 25 51200 2 ../../csv/ntest_d3N25to51200interval2.csv &
+#./ntest.out 3 25 51200 2 ../../csv/ntest_d3N25to51200interval2.csv &
 
 #./alttest.out 1000 3 ../../csv/alttest_N1000d3.csv & 
 #./alttest.out 10000 3 ../../csv/alttest_N10000d3.csv & 
